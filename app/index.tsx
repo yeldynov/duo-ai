fca251f80c9f35fcb1659d4c69aa7e92c2ff4879
Nikita Yeldynov <yeldynov@gmail.com>
2026-05-18 16:20:19 +0300

    update: change app title to 'Lingua'

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native'
 export default function Index() {
   return (
     <View className='flex-1 justify-center items-center'>
-      <Text className='h1 text-center color-lingua-purple'>Duolingo Clone</Text>
+      <Text className='h1 text-center color-lingua-purple'>Lingua</Text>
     </View>
   )
 }
